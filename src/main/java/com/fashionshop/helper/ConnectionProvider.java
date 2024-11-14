7aec3857213702455603d11f276bade27d3f293e
TienNHM.Utop <TienNHM@utop.io>
2024-11-14 12:08:50 +0700

    update connectionString

--- src/main/java/com/fashionshop/helper/ConnectionProvider.java
+++ src/main/java/com/fashionshop/helper/ConnectionProvider.java
@@ -14,7 +14,8 @@ public class ConnectionProvider extends HttpServlet{
 		try {
 			if (connection == null) {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fashionshop", "root", "root");
+				connection = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12744502", "sql12744502", "EDYMwjjVNL");
+				// connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fashionshop", "root", "root");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
